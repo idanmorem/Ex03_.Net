5d017b1afca027662190f50b1b2307826a8592e0
idanmorem <37801660+idanmorem@users.noreply.github.com>
2021-08-16 20:11:49 +0300

    Add files via upload

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex03.GarageLogic
+{
+     public abstract class Vehicle
+     {
+          private string m_ModelName;
+          private readonly string r_LiscenceNumber;
+          private float m_EnergyPercent;
+          private Wheel* m_Wheels; // array of Wheels
+          private readonly Wheel.eNumberOfWheels r_NumberOfWheels;
+          private string m_OwnersName;
+          private string m_OwnersPhoneNumber;
+          private eVehicleStatus m_Status;
+          private Engine m_Engine;
+
+          public enum eVehicleStatus
+          {
+               InProgress,
+               Fixed,
+               Paid
+          }
+
+     }
+}
